aff33c781b465f4abc289d6c274a842bfc28f687
Joe Heitkamp <noetic97@gmail.com>
2017-08-19 22:31:44 -0600

    add knex configuration to test suite

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,15 +1,11 @@
+const configuration = require('../knexfile')['test'];
+const database = require('knex')(configuration);
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
-// const seedData = require('../db/seeds/dev/folders')
 
 chai.use(chaiHttp);
-// beforeEach((done) => {
-//   console.log(seedData.seed());
-//   console.log(server);
-//   done()
-// })
 
 describe('Client Routes', () => {
   it('Should return the home page with text', (done) => {
@@ -22,6 +18,17 @@ describe('Client Routes', () => {
       done();
     })
   });
+});
+
+describe('API Routes', () => {
+  before(done => {
+    database.migrate.latest().then(() => done())
+  });
+
+  beforeEach(done => {
+    database.seed.run()
+    .then(() => done())
+  });
 
   it('Should return all of the folders in the DB', (done) => {
     chai.request(server)
@@ -30,14 +37,14 @@ describe('Client Routes', () => {
       response.should.have.status(200);
       response.should.be.json;
       response.body.should.be.a('array');
-      // response.body.length.should.equal(3);
+      response.body.length.should.equal(1);
       response.body[0].should.have.property('id');
-      // response.body[0].id.should.equal('1');
+      response.body[0].id.should.equal(17);
       response.body[0].should.have.property('title');
-      // response.body[0].title.should.equal('1');
+      response.body[0].title.should.equal('Favorites');
       response.body[0].should.have.property('created_at');
       response.body[0].should.have.property('updated_at');
       done();
-    })
-  })
+    });
+  });
 });

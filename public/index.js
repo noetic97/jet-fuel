909ca1b7e07687c0b220d95ffaf36db1bdbf9616
Joe Heitkamp <noetic97@gmail.com>
2017-08-21 08:41:21 -0600

    add functionality

--- public/index.js
+++ public/index.js
@@ -1,10 +1,40 @@
 const folderSubmit = $('.folder-submit');
 const folderNamer = $('.folder-namer');
-const deleteLink =$('.delete-link')
-const deleteFolder =$('.delete-folder')
+const deleteLink = $('.delete-link')
+const deleteFolder = $('.delete-folder')
+const folderSelect = $('.folder-button')
+
+
+const getFolders = () => {
+  fetch('api/v1/folders')
+  .then(res => res.json())
+  .then(data => appendFolders(data))
+  .catch(err => displayError(err))
+}
+
+const displayError = (error) => {
+  console.log(error);
+  $('#error-display').append(`<div>error</div>`)
+}
+
+const appendFolders = (data) => {
+  $('.folder-display').append(data.map((folder) =>
+      `<div>
+        <p class="folder-title">${folder.title}</p>
+        <button class="folder-button">
+          <img class="folder-img"
+               src="./assets/wood-folder.ico"
+               alt="opening and closing folder">
+        </button>
+      </div>`
+    ))
+}
+
+
 
 folderSubmit.click((e) => {
   e.preventDefault();
+  console.log(e);
 })
 
 folderNamer.change((e) => {
@@ -16,7 +46,12 @@ folderNamer.change((e) => {
     body: JSON.stringify({ title: folderNamer.val() })
   })
   .then(res => res.json())
-  .then(data => console.log(data.title))
+  .then(data => {
+    const folderArray = []
+    folderArray.push(data)
+    appendFolders(folderArray)
+  })
+  .catch(err => displayError(err))
   folderNamer.val('');
 })
 
@@ -31,23 +66,10 @@ deleteFolder.click((e) => {
   })
 })
 
+$('.folder-image').click(() => {
+  console.log('hi');
+  console.log(e);
+})
 
-// app.delete('/api/v1/folders/:id', (req, res) => {
-//   const { id } = req.params;
-//
-//   database('folders').where({ id }).del()
-//     .then(res => {
-//       console.log(res[0].id);
-//       const { folder_id } = res[0].id
-//       return database('links').where({ folder_id }).del()
-//     })
-//     .catch(err => res.status(404).json({ err: `The folder with id ${id} was not found` }));
-// });
-//
-// app.delete('/api/v1/links/:id', (req, res) => {
-//   const { id } = req.params;
-//
-//   database('links').where({ id }).del()
-//   .then(res => res.status(200).json())
-//   .catch(err => res.status(404).json({ error: err }))
-// });
+// Page load
+getFolders()

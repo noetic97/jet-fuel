08c6e9b7d45929a404544f80212827a4b860377d
Joe Heitkamp <noetic97@gmail.com>
2017-08-21 10:15:52 -0600

    fix test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -39,7 +39,7 @@ describe('API Routes', () => {
         response.should.have.status(200);
         response.should.be.json;
         response.body.should.be.a('array');
-        response.body.length.should.equal(1);
+        response.body.length.should.equal(2);
         response.body[0].should.have.property('id');
         response.body[0].id.should.not.be.NaN;
         response.body[0].id.should.equal(1);

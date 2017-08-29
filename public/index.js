913b7cb0d22d39b88ba269bb95d93ed21f623b99
Joe Heitkamp <noetic97@gmail.com>
2017-08-29 03:17:12 -0600

    add link display

--- public/index.js
+++ public/index.js
@@ -3,10 +3,10 @@ const folderNamer = $('.folder-namer');
 const linkNamer = $('.link-input')
 const linkDescription = $('.description-input')
 const folderSelect = $('select')
-
+const folderDisplay = $('.folder-display')
 
 const getFolders = () => {
-  fetch('api/v1/folders')
+  fetch('/api/v1/folders')
   .then(res => res.json())
   .then(folders => appendFolders(folders))
   .catch(err => displayError(err))
@@ -16,13 +16,15 @@ const displayError = (error) => {
   $('#error-display').append(`<div>ERROR: ${error}</div>`)
 }
 
-const folderWrapper = (folder) => `<div class="folder" id="${folder.id}">
-                                     <p id="folder-title">${folder.title}</p>
+const folderWrapper = (folder) => `<div class="folder" id=${folder.id}>
+                                     <p id=${folder.id}>${folder.title}</p>
                                      <img class="folder-img"
+                                          id=${folder.id}
                                           src="./assets/wood-folder.ico"
                                           alt="wood folder">
                                    </div>`;
 const folderOption = (folder) => `<option value=${folder.id}>${folder.title}</option>`;
+const linkWrapper = (link) => `<li id=${link.folder_id}>${link.description}: ${link.ogURL}</li>`;
 
 
 const appendFolders = (folders) => {
@@ -30,11 +32,16 @@ const appendFolders = (folders) => {
   $('#folder-select').append(folders.map(folderOption));
 };
 
+const appendLinks = (links) => {
+  $('.link-list').append(links.map(linkWrapper));
+}
+
 folderSubmit.click((e) => {
   e.preventDefault();
 });
 
-folderSelect.change((e) => {
+
+const toggleInputs = (e) => {
   if (e.target.value === 'Add a new folder') {
     folderNamer.show()
     linkDescription.hide()
@@ -46,9 +53,10 @@ folderSelect.change((e) => {
     linkDescription.show()
     linkNamer.show()
   }
-});
+}
 
-folderNamer.change((e) => {
+const addFolder = (e) => {
+  e.preventDefault()
   fetch('/api/v1/folders', {
     method: 'POST',
     headers: {
@@ -71,10 +79,10 @@ folderNamer.change((e) => {
     }
   })
   .catch(err => displayError(err))
+}
 
-})
-
-linkNamer.change((e) => {
+const addLink = (e) => {
+  e.preventDefault()
   const folder_id = $('#folder-select').val()
   fetch('/api/v1/links', {
     method: 'POST',
@@ -88,16 +96,34 @@ linkNamer.change((e) => {
     })
   })
   .then(res => res.json())
-  .then(data => {
-
-    console.log(data)
+  .then(links => {
+    const linkArray = []
+    linkArray.push(links)
+    appendLinks(linkArray)
   })
+  .catch(err => displayError(err))
   linkNamer.val('')
   linkDescription.val('')
-})
+}
+
+
+const displayLinks = (e) => {
+  const id = e.target.id;
+  fetch(`api/v1/folders/${id}/links`)
+    .then(res => res.json())
+    .then(links => appendLinks(links))
+    .catch(err => displayError(err))
+    $('.folder-display').find('.link-display').toggleClass('hidden-display');
+}
 
 // Page load
 getFolders()
 folderNamer.hide()
 linkNamer.hide()
 linkDescription.hide()
+
+// Event Listeners
+folderDisplay.on('click', '.folder', displayLinks)
+folderSelect.on('change', toggleInputs)
+folderNamer.on('change', addFolder)
+linkNamer.on('change', addLink)
